5379b88ec3f6a32efe741b399ad2e8b2202f9481
Lucerovv <lu.varvil21@gmail.com>
2021-11-26 09:41:54 -0600

    agregar tarea
    agregar tarea

--- src/app/pages/sucursales/add-tareas/add-tareas.component.ts
+++ src/app/pages/sucursales/add-tareas/add-tareas.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialog,
+} from "@angular/material/dialog";
+import { ServiceGeneralService } from "./../../../core/services/service-general/service-general.service";
 
 @Component({
-  selector: 'app-add-tareas',
-  templateUrl: './add-tareas.component.html',
-  styleUrls: ['./add-tareas.component.css']
+  selector: "app-add-tareas",
+  templateUrl: "./add-tareas.component.html",
+  styleUrls: ["./add-tareas.component.css"],
 })
-export class AddTareasComponent implements OnInit {
 
-  constructor() { }
+export class AddTareasComponent implements OnInit {
+  public disabled = false;
+  public today = new Date();
+  public user;
+  public data: any[] = [];
+  // ***********variables de validacion***********
+  // ******variables de validacion ********
+  public activeNombre = false;
+  public activeTurno = false;
+  public activeDescripcion = false;
+  public activeSupervisor = false;
+  public activeSucursal = false;
+
+
+  constructor(public dialogRef: MatDialogRef<AddTareasComponent>,
+    @Inject(MAT_DIALOG_DATA) public param: any,
+    public services: ServiceGeneralService,
+    public _dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
